450a2c47ba7754d64ba16f84f3a63d12ead3e70a
Sepehr <alisepehr88@gmail.com>
2026-01-10 14:32:09 +0100

    Added vendors
    Signed-off-by: Sepehr <alisepehr88@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
+#include "Common/BasicDataTypes.h"
+#include "Common/NameManglingGuard.h"
 #include <stdio.h>
+
+Common_NameManglingGuard_Begin
 
 int main()
 {
-    printf("Hello, World.\r\n");
+    const UInt8 testingTheCommonLib = 2;
+    printf("The test value is: %u\r\n", testingTheCommonLib);
     return 0;
 }
+
+Common_NameManglingGuard_End
